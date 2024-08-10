84eb97f4504aaf331a49eb99f2d45b5ebceeac19
aziz-hij <82012061+aziz-hij@users.noreply.github.com>
2024-08-10 09:15:01 +0700

    initial project

---
+++ src/main/java/com/aziz/unsia/UnsiaApplication.java
@@ -0,0 +1,13 @@
+package com.aziz.unsia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aziz/unsia/UnsiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aziz.unsia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
